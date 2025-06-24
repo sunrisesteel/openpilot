a6170acf78f35e85e54be7056fdcda1c65ec885e
sunrisesteel <111093089+sunrisesteel@users.noreply.github.com>
2025-06-24 09:24:56 +0100

    removed pands requirement on can_table.py

--- selfdrive/debug/can_table.py
+++ selfdrive/debug/can_table.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 import argparse
-import pandas as pd
 
 import cereal.messaging as messaging
 
 
 def can_table(dat):
+  header = [str(n) for n in range(7, -1, -1)] + [' ']
   rows = []
   for b in dat:
-    r = list(bin(b).lstrip('0b').zfill(8))
-    r += [hex(b)]
-    rows.append(r)
-
-  df = pd.DataFrame(data=rows)
-  df.columns = [str(n) for n in range(7, -1, -1)] + [' ']
-  table = df.to_markdown(tablefmt='grid')
+    bits = list(bin(b)[2:].zfill(8))
+    row = bits + [hex(b)]
+    rows.append(row)
+  col_widths = [max(len(header[i]), max(len(row[i]) for row in rows)) for i in range(len(header))]
+  header_line = " | ".join(header[i].rjust(col_widths[i]) for i in range(len(header)))
+  sep_line = "-+-".join('-' * col_widths[i] for i in range(len(header)))
+  row_lines = []
+  for row in rows:
+    row_lines.append(" | ".join(row[i].rjust(col_widths[i]) for i in range(len(row))))
+  table = f"{header_line}\n{sep_line}\n" + "\n".join(row_lines)
   return table
 
 
